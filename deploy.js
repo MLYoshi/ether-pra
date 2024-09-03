6ec30b9a34099546eb4540afa243732d0a0fda6d
yoshi <pbl2680658669@gmail.com>
2024-09-03 15:50:19 +0800

    from mac

--- deploy.js
+++ deploy.js
@@ -1,16 +1,14 @@
 const ethers = require("ethers");
 const fs = require("fs");
+require("dotenv").config();
 
 async function main() {
   const provider = new ethers.providers.JsonRpcProvider(
-    "http://127.0.0.1:7545",
+    process.env.URL,
     null,
     { cacheTimeout: 5000 } // 你的 RPC 服务器地址
   );
-  const wallet = new ethers.Wallet(
-    "0x771f7d1517686830bf002b3074c3f82b555c05300b212af38ab2e830785a4ee8",
-    provider
-  );
+  const wallet = new ethers.Wallet(process.env.PrivateKey, provider);
 
   const abi = fs.readFileSync("./SimpleStorage_sol_SimpleStorage.abi", "utf8");
   const binary = fs.readFileSync(
